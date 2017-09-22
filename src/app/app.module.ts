08f738bc3d09f152c9be97a0cf927a64becd27eb
rozborsky <roman.rozborsky@gmail.com>
2017-09-22 21:41:40 +0300

    add module 'registration'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ModalModule } from "ngx-modal";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,6 +12,12 @@ import { MessageFormComponent } from './message-form/message-form.component';
 import { MessageComponent } from './message/message.component';
 import { TextCutting } from './text-cutting.pipe';
 import { MessageMenuComponent } from './message-menu/message-menu.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const appRoutes: Routes =[
+  { path: '', component: ContentComponent},
+  { path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   declarations: [
@@ -20,13 +27,15 @@ import { MessageMenuComponent } from './message-menu/message-menu.component';
     MessageFormComponent,
     MessageComponent,
     TextCutting,
-    MessageMenuComponent
+    MessageMenuComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     ModalModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
